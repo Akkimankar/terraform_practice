58f5fefd850f850ca8fb9c77fdc1f480599dc83d
Hitesh Mankar <Hiteshmankar777@gmail.com>
2022-06-28 00:38:28 +0530

    --- Auto Git Commit ---

--- MyFirstScript.tf
+++ MyFirstScript.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "web-server" {
   ami = "ami-0cff7528ff583bf9a"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   security_groups = ["default"]
   key_name = "terraform"
 }
