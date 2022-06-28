23568de790f8a74b000aa2f03cace5410564855f
Hitesh Mankar <Hiteshmankar777@gmail.com>
2022-06-28 09:33:14 +0530

    --- Auto Git Commit ---

---
+++ main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "web-server" {
+  ami = "ami-0cff7528ff583bf9a"
+  instance_type = "t3.micro"
+  security_groups = ["default"]
+  key_name = "terraform"
+}
+
+resource "aws_instance" "app-server" {
+  ami = "ami-0cff7528ff583bf9a"
+  instance_type = "t2.micro"
+  security_groups = ["default"]
+  key_name = "terraform"
+}
