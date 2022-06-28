5a976acdee76f9bb0c94aa8afcd28465d2bc5ce3
Hitesh Mankar <Hiteshmankar777@gmail.com>
2022-06-28 09:39:26 +0530

    --- Auto Git Commit ---

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "web-server" {
 
 resource "aws_instance" "app-server" {
   ami = var.ami_id
-  instance_type = "t2.micro"
+  instance_type = var.ami_id
   security_groups = ["default"]
   key_name = "terraform"
 }
