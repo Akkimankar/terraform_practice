6507ebec5a50a7b5cbc7bfbff038d59ab4719c3f
Hitesh Mankar <Hiteshmankar777@gmail.com>
2022-06-28 19:45:31 +0530

    --- Auto Git Commit ---

--- terraform-practice/main.tf
+++ terraform-practice/main.tf
@@ -28,7 +28,7 @@ resource "aws_internet_gateway" "igw"  {
   vpc_id = aws_vpc.vpc1.id
 }
 
-resource "aws_internet_gateway_attachment" "" {
+resource "aws_internet_gateway_attachment" "igw-" {
   
 }
 
