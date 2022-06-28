ab92372053b24793d9644cb3f73855e39d766abd
Hitesh Mankar <Hiteshmankar777@gmail.com>
2022-06-28 10:15:36 +0530

    --- Auto Git Commit ---

--- variable.tf
+++ variable.tf
@@ -1,14 +1,17 @@
 variable "ami_id" {
-    type = string
-    default = "ami-0cff7528ff583bf9a"
-    description = "AMI-ID"
+  type = string
+  default = "ami-0022f774911c1d690"
+  description = "AMI-ID"
 }
 
 variable "instance_type" {
-default = " t2.micro" 
-} 
+  default = "t2.micro"
+}
 
 variable "vpc_id" {
-    default = ""
+    default = "vpc-00a9701be112fe95c" 
+}
 
+variable "key_name" {
+  default = "aws-nv-cbz"
 }
