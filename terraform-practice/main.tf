1d4129352d951a25b81c3fe33cd087d0d4b929c0
Hitesh Mankar <Hiteshmankar777@gmail.com>
2022-06-28 19:41:48 +0530

    --- Auto Git Commit ---

--- terraform-practice/main.tf
+++ terraform-practice/main.tf
@@ -17,7 +17,7 @@ resource "aws_subnet" "private_subnet" {
 resource "aws_subnet" "private_subnet" {
   vpc_id = aws_vpc.vpc1.id
   cidr_block = "10.10.0.0/20"
-  availability_zone = 
+  availability_zone = "us-east-1b"
   map_public_ip_on_launch = "true"
   tags ={
     "Name" = "public_subnet"
