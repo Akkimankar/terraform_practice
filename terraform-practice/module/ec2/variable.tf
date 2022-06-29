94b741d9b16da5be81d486f97fafb87a6416ff43
Hitesh Mankar <Hiteshmankar777@gmail.com>
2022-06-29 08:27:15 +0530

    --- Auto Git Commit ---

--- terraform-practice/module/ec2/variable.tf
+++ terraform-practice/module/ec2/variable.tf
@@ -3,10 +3,11 @@ variable "ami" {
 }
 
 variable "instance_type" {
-    default = ""  
+    default = "t2.micro"  
 }
 
 variable "key_name" {
+    default = "terraform"
 }
 
 variable "private_subnet_id" {
