68b5f05d751f661f05c312903c393030fc421c17
Hitesh Mankar <Hiteshmankar777@gmail.com>
2022-08-29 07:57:55 +0530

    --- Auto Git Commit ---

--- terraform-practice/module/vpc/main.tf
+++ terraform-practice/module/vpc/main.tf
@@ -62,7 +62,18 @@ resource "aws_internet_gateway" "gw" {
   internet_gateway_id = aws_internet_gateway
 }
 
-resource "aws_default_"
+resource "aws_route_table" "example" {
+  vpc_id = aws_vpc.example.id
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.example.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    egress_only_gateway_id = aws_egress_only_internet_gateway.example.id
+  }
 
 
 
