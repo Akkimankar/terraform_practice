35886a96e48f574f5eae3b1e6ad6b8a68b5897f7
Hitesh Mankar <Hiteshmankar777@gmail.com>
2022-08-29 07:48:52 +0530

    --- Auto Git Commit ---

--- terraform-practice/module/vpc/main.tf
+++ terraform-practice/module/vpc/main.tf
@@ -52,7 +52,14 @@ resource "aws_vpc" "main" {
     "Name" = "vpc"
 }
 
+esource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
 
+  tags = {
+    Name = "Main"
+  }
+}
 
 
 
