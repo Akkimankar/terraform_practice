6634b4fe42d77bf191054001619a39863348bff5
Hitesh Mankar <Hiteshmankar777@gmail.com>
2022-08-29 07:59:56 +0530

    --- Auto Git Commit ---

--- terraform-practice/module/vpc/main.tf
+++ terraform-practice/module/vpc/main.tf
@@ -66,7 +66,7 @@ resource "aws_route_table" "example" {
   vpc_id = aws_vpc.example.id
 
   route {
-    cidr_block = "10.0.0.0/0"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.example.id
   }
 
