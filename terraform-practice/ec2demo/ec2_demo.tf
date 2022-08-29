0f54e8e80a62d11c3a35228ca8c12836ff90d929
Hitesh Mankar <Hiteshmankar777@gmail.com>
2022-08-29 07:17:43 +0530

    --- Auto Git Commit ---

--- terraform-practice/ec2demo/ec2_demo.tf
+++ terraform-practice/ec2demo/ec2_demo.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web-server" {
   ami = "var.ami_id"  
-  instance_type = "var.instance"
+  instance_type = "var.instance_type"
   security_groups = ["default"]
   key_name = "devops-key"
  }
